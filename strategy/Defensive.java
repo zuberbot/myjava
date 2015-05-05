ee8b14c3df7632987ac433f868a7226923079afc
Chuck Thompson <zuberbot@users.noreply.github.com>
2015-05-04 20:50:12 -0700

    Create Defensive.java

---
+++ strategy/Defensive.java
@@ -0,0 +1,19 @@
+/**
+ * Defensive
+ */
+package com.java.strategy;
+
+/**
+ * @author Chuck
+ *
+ */
+public class Defensive implements IBehavior {
+
+	/**
+	 * 
+	 */
+	public void move() {
+		System.out.println("I'm protecting myself.");
+	}
+
+}
