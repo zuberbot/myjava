b3499eca1a4ca8e1dade80fb5abe7f0ffe65def4
Chuck Thompson <zuberbot@users.noreply.github.com>
2015-05-04 20:50:42 -0700

    Create Submissive.java

---
+++ strategy/Submissive.java
@@ -0,0 +1,19 @@
+/**
+ * Submissive
+ */
+package com.java.strategy;
+
+/**
+ * @author Chuck
+ *
+ */
+public class Submissive implements IBehavior {
+
+	/**
+	 * 
+	 */
+	public void move() {
+		System.out.println("Just going with the flow.");
+	}
+
+}
