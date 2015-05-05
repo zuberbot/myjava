fcd163239814c52e569ec1b2566a904cf5146c12
Chuck Thompson <zuberbot@users.noreply.github.com>
2015-05-04 20:49:50 -0700

    Create Aggressive.java

---
+++ strategy/Aggressive.java
@@ -0,0 +1,19 @@
+/**
+ * Aggressive
+ */
+package com.java.strategy;
+
+/**
+ * @author Chuck
+ *
+ */
+public class Aggressive implements IBehavior {
+
+	/**
+	 * 
+	 */
+	public void move() {
+		System.out.println("I'm going to knock you down!");
+	}
+
+}
