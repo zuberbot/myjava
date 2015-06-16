a5d835916797fa5b4e546564190a8b9fccc1d410
Chuck Thompson <zuberbot@users.noreply.github.com>
2015-06-15 19:00:58 -0700

    Create TestMe.java

---
+++ interfaces/TestMe.java
@@ -0,0 +1,22 @@
+/**
+ * 
+ */
+package com.java.interfaces;
+
+/**
+ * @author Chuck
+ *
+ */
+public class TestMe {
+
+	/**
+	 * 
+	 */
+	public TestMe() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public int computer(int x) {
+		return x * 2;
+	}
+}
