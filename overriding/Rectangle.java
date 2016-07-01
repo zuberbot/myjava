bed54c839be0f1c22184da2b88d51e12eee83cb3
Chuck Thompson <zuberbot@users.noreply.github.com>
2016-07-01 12:40:33 -0700

    Create Rectangle.java

---
+++ overriding/Rectangle.java
@@ -0,0 +1,18 @@
+package overriding;
+
+/**
+ *
+ * @author Chuck
+ */
+public class Rectangle extends Figure {
+
+    Rectangle(double a, double b) {
+        super(a, b);
+    }
+
+    @Override
+    double area() {
+        System.out.println("Inside Area for Rectangle.");
+        return dim1 * dim2;
+    }
+}
