1c512fbd41917241fa86e81c21aaa22556071cab
Chuck Thompson <zuberbot@users.noreply.github.com>
2016-07-01 12:16:34 -0700

    Create HouseFactory.java

---
+++ abstractfactory/HouseFactory.java
@@ -0,0 +1,16 @@
+package abstractfactory;
+
+/**
+ *
+ * @author Chuck
+ */
+public class HouseFactory {
+    
+    public void createBulb() {
+        // Implementation deferred to specific subclass
+    }
+    
+    public void createBlind() {
+        // Implementation deferred to specific subclass
+    }
+}
