64bb6c699a8dcdf0fa0bab89142160db16c12033
Chuck Thompson <zuberbot@users.noreply.github.com>
2016-07-01 12:40:04 -0700

    Create FindAreas.java

---
+++ overriding/FindAreas.java
@@ -0,0 +1,30 @@
+package overriding;
+
+/**
+ *
+ * @author Chuck
+ */
+public class FindAreas {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String... args) {
+
+        Figure f = new Figure(10, 10);
+        Rectangle r = new Rectangle(9, 5);
+        Triangle t = new Triangle(10, 8);
+        
+        Figure figref;
+        
+        figref = r;
+        System.out.println("Area is " + figref.area());
+
+        figref = t;
+        System.out.println("Area is " + figref.area());
+
+        figref = f;
+        System.out.println("Area is " + figref.area());
+    }
+
+}
