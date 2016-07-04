6fad8f4e8f20ca08fcd8dc0b44f60d8c27224fe4
Chuck Thompson <zuberbot@users.noreply.github.com>
2016-07-04 08:46:13 -0700

    Update SmallFiles.java

--- files/SmallFiles.java
+++ files/SmallFiles.java
@@ -22,25 +22,23 @@ public class SmallFiles {
 		
 		String root = File.listRoots()[0].getAbsolutePath();
 		String path = root + File.separator + "temp" + File.separator;
+		String path1 = path + "smallfileR.txt";
+		String path2 = path + "smallfileW.txt";
 		
 		try {
-			path += "smallfileR.txt";
-			Path file = Paths.get(path);
-			byte[] fileArray = Files.readAllBytes(file);
+			// Read from file
+			Path file1 = Paths.get(path1);
+			byte[] fileArray = Files.readAllBytes(file1);
 			String content = new String(fileArray, "UTF-8");
 			System.out.println(content);
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-		
-		try {
-			path += "smallfileW.txt";
-			Path file = Paths.get(path);
+
+			// Write from file
+			Path file2 = Paths.get(path2);
 			String str = "This is my sample content 123.";
 			byte[] buf = str.getBytes();
-			Files.write(file, buf);
-		} catch (IOException e) {
-			e.printStackTrace();
+			Files.write(file2, buf);
+		} catch (IOException ioe) {
+			ioe.printStackTrace();
 		}
 	}
 }
