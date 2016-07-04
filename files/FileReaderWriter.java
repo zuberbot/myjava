d1fd0e3e420fe81c16e4415783cd27d143d22e11
Chuck Thompson <zuberbot@users.noreply.github.com>
2016-07-04 08:33:32 -0700

    Create FileReaderWriter.java

---
+++ files/FileReaderWriter.java
@@ -0,0 +1,45 @@
+/**
+ * FileReaderWriter
+ */
+package com.planet.file;
+
+import java.io.File;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+
+/**
+ * @author chuck
+ *
+ */
+public class FileReaderWriter {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String... args) {
+
+		String root = File.listRoots()[0].getAbsolutePath();
+		String path = root + File.separator + "temp" + File.separator;
+		String file1 = path + "fileR.txt";
+		String file2 = path + "fileW.txt";
+		
+		// Read file
+		try (FileReader fr = new FileReader(file1)) {
+			int c;
+			while ((c = fr.read()) != -1) {
+				System.out.print((char) c);
+			}
+		} catch (IOException ioe) {
+		    ioe.printStackTrace();
+		}
+
+		// Write file
+		String s = "Write this message to file 123.";
+		try (FileWriter fw = new FileWriter(file2)) {
+			fw.write(s);
+		} catch (IOException ioe) {
+		    ioe.printStackTrace();
+		}
+	}
+}
